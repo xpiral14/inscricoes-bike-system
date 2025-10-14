0aaa01a509c774e4f59fb7bcf8ef2bca4a5d7bc9
xpiral14 <sa.reis1999@gmail.com>
2025-10-14 16:42:20 -0300

    adicionado gerenciador de relação para pagamentos em EventoResource

---
+++ app/Filament/Resources/Eventos/RelationManagers/PagamentosRelationManager.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Filament\Resources\Eventos\RelationManagers;
+
+use Filament\Actions\AssociateAction;
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\CreateAction;
+use Filament\Actions\DeleteAction;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\DissociateAction;
+use Filament\Actions\DissociateBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Actions\ViewAction;
+use Filament\Forms\Components\TextInput;
+use Filament\Infolists\Components\TextEntry;
+use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Schemas\Schema;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+
+class PagamentosRelationManager extends RelationManager
+{
+    protected static string $relationship = 'pagamentos';
+
+    public function form(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('Pagamento')
+                    ->required()
+                    ->maxLength(255),
+            ]);
+    }
+
+    public function infolist(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextEntry::make('Pagamento'),
+            ]);
+    }
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->recordTitleAttribute('Pagamento')
+            ->columns([
+                TextColumn::make('Pagamento')
+                    ->searchable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->headerActions([
+                CreateAction::make(),
+                AssociateAction::make(),
+            ])
+            ->recordActions([
+                ViewAction::make(),
+                EditAction::make(),
+                DissociateAction::make(),
+                DeleteAction::make(),
+            ])
+            ->toolbarActions([
+                BulkActionGroup::make([
+                    DissociateBulkAction::make(),
+                    DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
