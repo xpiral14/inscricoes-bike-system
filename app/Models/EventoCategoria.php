76ccb8eb08c0e3738adc2efc6fae92cf6d011d70
xpiral14 <sa.reis1999@gmail.com>
2025-10-14 15:45:26 -0300

    adicionados modelos EventoCategoria, EventoInscritoSituacao e Log

---
+++ app/Models/EventoCategoria.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class EventoCategoria extends Model
+{
+    protected $table = 'tb_event_categories';
+}
---
+++ app/Models/EventoInscritoSituacao.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class EventoInscritoSituacao extends Model
+{
+    protected $table = 'tb_eventos_inscritos_situacaoes';
+}
