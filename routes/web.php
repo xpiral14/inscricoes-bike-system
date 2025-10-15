53a6ce4d5f3db52e73272cf084f6efe9a2289abe
xpiral14 <sa.reis1999@gmail.com>
2025-10-15 16:50:53 -0300

    atualizadas rotas web: substituída view 'welcome' pelo método index do HomeController; adicionadas rotas para detalhes de evento e processamento de inscrição

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\EventoController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
+// Rota para exibir a página de detalhes do evento
+Route::get('/eventos/{evento}', [EventoController::class, 'show'])->name('eventos.show');
+
+// Rota para processar a solicitação de inscrição (vamos usar mais tarde)
+Route::post('/inscricoes', [EventoController::class, 'processarInscricao'])->name('inscricoes.store');
