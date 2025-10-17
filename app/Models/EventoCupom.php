e02034ea3cbf9de99879e8537c36498ec05c44ad
xpiral14 <sa.reis1999@gmail.com>
2025-10-17 16:52:34 -0300

    adicionados modelos 'Cupom' e 'EventoCupom' com relações associadas

---
+++ app/Models/EventoCupom.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class EventoCupom extends Model
+{
+    protected $table = 'tb_eventos_cupons';
+
+
+    public function evento()
+    {
+        return $this->belongsTo(Evento::class, 'evento');
+    }
+
+    public function criador()
+    {
+
+        return $this->belongsTo(Usuario::class, 'criadopor');
+    }
+
+    public function usuario()
+    {
+
+        return $this->belongsTo(Usuario::class, 'usadorpor');
+    }
+}
