318e932e5fe83732496fe552272515c00e8d4866
xpiral14 <sa.reis1999@gmail.com>
2025-10-19 19:27:56 -0300

    adicionadas rotas de API para autenticação, gestão de inscrições e listagem de estados e cidades

---
+++ routes/api.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+Route::get('/cidades/{estado}', [\App\Http\Controllers\LocationController::class, 'listaDeCidades'])->name('cidades.list');
+Route::get('/estados', [\App\Http\Controllers\LocationController::class, 'listaDeEstados'])->name('estados.list');
+Route::post('/login', [\App\Http\Controllers\SessionController::class, 'login'])->name('api.login');
+Route::post('/register', [\App\Http\Controllers\SessionController::class, 'register'])->name('api.register');
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::post('/inscricoes', [\App\Http\Controllers\EventoController::class, 'processarInscricao'])->name('inscricoes.store');
+});
+
