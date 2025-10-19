8f270ad4a73633745c069a9ff3176d2c446bd1dd
xpiral14 <sa.reis1999@gmail.com>
2025-10-19 19:27:08 -0300

    ajustada rota de login e removida rota não utilizada para processar inscrições

--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
+Route::get('/login', [HomeController::class, 'index'])->name('login');
+
 // Rota para exibir a página de detalhes do evento
 Route::get('/eventos/{evento}', [EventoController::class, 'show'])->name('eventos.show');
 
-// Rota para processar a solicitação de inscrição (vamos usar mais tarde)
-Route::post('/inscricoes', [EventoController::class, 'processarInscricao'])->name('inscricoes.store');
