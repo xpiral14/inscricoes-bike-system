6b1627c556c22d2468ccf482c9a79c396dfc3d9e
xpiral14 <sa.reis1999@gmail.com>
2025-10-20 14:22:47 -0300

    ajustado modelo 'EventoCategoria' com desabilitação de timestamps, definição de casts e relação com 'Evento'

--- app/Models/EventoCategoria.php
+++ app/Models/EventoCategoria.php
@@ -7,4 +7,16 @@ use Illuminate\Database\Eloquent\Model;
 class EventoCategoria extends Model
 {
     protected $table = 'tb_event_categories';
+    public $timestamps = false;
+    protected $casts = [
+        'price' => 'float',
+        'ativo' => 'boolean',
+        'anoInicial' => 'integer',
+        'anoFinal' => 'integer',
+    ];
+
+    public function evento()
+    {
+        return $this->belongsTo(Evento::class, 'event'); // Assumindo que seu modelo de Evento se chama Evento
+    }
 }
